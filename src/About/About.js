fe83e5dd51ffdab4b9af69d8b5b63c63abdc6890
GlendyT <glendytuyuc@gmail.com>
2024-01-23 19:12:22 -0600

    updateimage

--- src/About/About.js
+++ src/About/About.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Avatar from '../Avatar/Avatar'
 import AboutMenu from './AboutMenu'
 
 export default class About extends Component {
   render() {
     return (
         <>
+        <Avatar page='about'/>
          <AboutMenu/>
         </>
     )
